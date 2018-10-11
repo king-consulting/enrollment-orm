56255408fc2bff776a9dcc90ba2787db7614a384
Greg Roberts <soulpainter@gmail.com>
2018-10-11 16:29:15 -0700

    removing temp password

--- bootstrap.php
+++ bootstrap.php
@@ -23,8 +23,8 @@ $conn = array(
 // the connection configuration
 $conn = array(
   'driver'   => 'pdo_mysql',
-  'user'     => 'root',
-  'password' => '&$#$JFl23asfjA)8wfLFr29&^',
+  'user'     => '__USER__',
+  'password' => '__PASSWORD__',
   'dbname'   => 'Enrollment',
 );
 
